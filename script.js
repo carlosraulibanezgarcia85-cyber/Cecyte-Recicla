53e7ce56ce5c5933c0b929a2bb5c0b69f491dc14
carlosraulibanezgarcia85-cyber <carlosraulibanezgarcia85@gmail.com>
2026-06-11 18:54:41 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function mensaje1(){
+    alert("El reciclaje transforma nuestro entorno y nuestras ideas.");
+}
+
+function mensaje2(){
+    alert("Cada corcholata reciclada ayuda a cuidar el planeta.");
+}
+
+function mensaje3(){
+    alert("Reutilizar materiales reduce la contaminación.");
+}
+
+function mensaje4(){
+    alert("El arte sustentable convierte residuos en oportunidades.");
+}
